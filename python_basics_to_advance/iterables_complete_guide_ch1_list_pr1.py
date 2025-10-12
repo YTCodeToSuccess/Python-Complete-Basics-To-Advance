fcb0566845592f15ea63734a554a8be44df6b048
Shubham Srivastava <shubhamsrivastava@Shubhams-Laptop.local>
2025-10-12 11:11:36 +0530

    ch-list-part1

---
+++ python_basics_to_advance/iterables_complete_guide_ch1_list_pr1.py
@@ -0,0 +1,21 @@
+'''
+This section we will cover everything about iterables in Python.
+Like lists, tuples, sets, dictionaries, strings, etc.
+and will see most common operations that can be performed on them.
+'''
+
+
+if __name__ == "__main__":
+
+    # list of number from 1 to 10
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # print on the even 
+    b = numbers[1: : 2]
+
+    print(b)  
+    
+    
+
+
+
+    
